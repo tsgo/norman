52f6d443db51040b28d8161202e9bb83ae9da8a2
SaSaChu <teresa7397@gmail.com>
2020-04-15 16:45:51 +0800

    修改完成

--- js/public.js
+++ js/public.js
@@ -1,9 +1,16 @@
-/**
- * @author      OA Wu <comdan66@gmail.com>
- * @copyright   Copyright (c) 2015 - 2019, Ginkgo
- * @license     http://opensource.org/licenses/MIT  MIT License
- * @link        https://www.ioa.tw/
- */
- 
+
 $(function() {
+
+     // 產品點選切換tab
+   $('.ba').click(function() {
+    $('.ba').removeClass ('h');
+    $(this).addClass ('h');
+
+    $('.p_boxs').removeClass('p_boxs_show');
+    $('.p_boxs').eq($(this).index()).addClass('p_boxs_show');
+   });
+
+   $('.ba').eq (0).click();
+
+
 });
